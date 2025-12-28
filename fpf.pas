a5c75041e25173ccd29a86799cf07aa83264c70c
Marie <marie@ha.si>
2025-12-28 14:25:13 +0100

    cleanup and fixes for static large images

--- fpf.pas
+++ fpf.pas
@@ -1,7 +1,7 @@
 {$mode fpc}
 program fpf;
 
-{$H+}{$R+}
+{$H+}{$R-}
 {$R resources.rc}
 
 uses classes, elfreader, math, resource, sockets, strutils, sysutils, types, uBMP;
@@ -22,29 +22,30 @@ type
 const
 	BSIZE = 10;
 
-procedure shuffle_block_array(var a: TBlockArray);
+procedure ShuffleBlockArray(var a: TBlockArray);
 var
-	i,j:integer;
-	t:TBlock;
+	i,j	: Integer;
+	t	: TBlock;
 begin
-	randomize;
-	i := length(a);
+	Randomize;
+	i := Length(a);
 	while i >1 do
 	begin
-		dec(i);
-		j :=randomrange(0,i);
-		t:=a[i];a[i]:=a[j];a[j]:=t;
+		Dec(i);
+		j	 := RandomRange(0,i);
+		t	 := a[i];
+		a[i] := a[j];
+		a[j] := t;
 	end;
 end;
 
-function make_block_array(pixels: TTexture): TBlockArray;
+function MakeBlockArray(pixels: TTexture): TBlockArray;
 var
 	nwblocks, nhblocks										: Extended;
 	{ rounded w/h blocks  remainder dimensions }
-	rnwblocks, rnhblocks, remainder_width, remainder_height	: UInt64;
-	total_wblocks, total_hblocks, total_blocks				: UInt64;
-
-	x, y, ox, oy, wx, wy, xbsize, ybsize, pix: UInt64;
+	rnwblocks, rnhblocks, remainderWidth, remainderHeight	: UInt64;
+	totalWBlocks, totalHBlocks, totalBlocks					: UInt64;
+	x, y, ox, oy, wx, wy, xbsize, ybsize, pix, n			: UInt64;
 begin
 	nwblocks := pixels.width / BSIZE;
 	nhblocks := pixels.height / BSIZE;
@@ -52,166 +53,159 @@ begin
 	rnwblocks := floor(nwblocks);
 	rnhblocks := floor(nhblocks);
 
-	remainder_width := round((nwblocks - rnwblocks) * bsize);
-	remainder_height := round((nhblocks - rnhblocks) * bsize);
+	remainderWidth := round((nwblocks - rnwblocks) * bsize);
+	remainderHeight := round((nhblocks - rnhblocks) * bsize);
 
-	total_wblocks := round(min(1.0, remainder_width)) + rnwblocks;
-	total_hblocks := round(min(1.0, remainder_height)) + rnhblocks;
-	total_blocks := total_wblocks * total_hblocks;
+	totalWBlocks := round(min(1.0, remainderWidth)) + rnwblocks;
+	totalHBlocks := round(min(1.0, remainderHeight)) + rnhblocks;
+	totalBlocks := totalWBlocks * totalHBlocks;
 
-	writeln('nwblocks: ', nwblocks);
-	writeln('nhblocks: ', nhblocks);
-	writeln('rnwblocks: ', rnwblocks);
-	writeln('rnhblocks: ', rnhblocks);
-	writeln('remainder w: ', remainder_width);
-	writeln('remainder h: ', remainder_height);
-	writeln('block dimension: ', total_wblocks, 'x', total_hblocks, ' (', total_blocks, ' total)');
+	WriteLn('[DEBUG] BSIZE: ', BSIZE);
+	WriteLn('[DEBUG] nwblocks: ', nwblocks);
+	WriteLn('[DEBUG] nhblocks: ', nhblocks);
+	WriteLn('[DEBUG] rnwblocks: ', rnwblocks);
+	WriteLn('[DEBUG] rnhblocks: ', rnhblocks);
+	WriteLn('[DEBUG] remainder w: ', remainderWidth);
+	WriteLn('[DEBUG] remainder h: ', remainderHeight);
+	WriteLn('[DEBUG] block dimension: ', totalWBlocks, 'x', totalHBlocks, ' (', totalBlocks, ' total)');
 
-	SetLength(make_block_array, total_blocks);
+	SetLength(MakeBlockArray, totalBlocks);
+	n := 0;
 
-	for y := 0 to total_hblocks - 1 do
+	for y := 0 to totalHBlocks - 1 do
 	begin
 		ybsize := bsize;
-		if y = rnhblocks then ybsize := remainder_height;
+		if y = rnhblocks then ybsize := remainderHeight;
 
-		wy := total_wblocks - 1;
-		for x := 0 to total_wblocks - 1 do
+		wy := totalWBlocks - 1;
+		for x := 0 to totalWBlocks - 1 do
 		begin
 			xbsize := bsize;
-			if x = rnwblocks then xbsize := remainder_width;
+			if x = rnwblocks then xbsize := remainderWidth;
 
-			SetLength(make_block_array[y * total_wblocks + x].data, xbsize * ybsize * 4);
-			make_block_array[y * total_wblocks + x].xorg := wy * xbsize;
-			make_block_array[y * total_wblocks + x].yorg := y * ybsize;
-			make_block_array[y * total_wblocks + x].w := xbsize;
-			make_block_array[y * total_wblocks + x].h := ybsize;
+			SetLength(MakeBlockArray[y * totalWBlocks + x].data, xbsize * ybsize * 4);
+			MakeBlockArray[y * totalWBlocks + x].xorg := wy * xbsize;
+			MakeBlockArray[y * totalWBlocks + x].yorg := y * ybsize;
+			MakeBlockArray[y * totalWBlocks + x].w := xbsize;
+			MakeBlockArray[y * totalWBlocks + x].h := ybsize;
+
+			Inc(n);
+			Write('[INFO] Preparing Block ', n, '/', Length(MakeBlockArray), #13);
 
 			for oy := 0 to ybsize - 1 do
 			begin
 				wx := xbsize - 1;
 				for ox := 0 to xbsize - 1 do
 				begin
-					pix := (((x * xbsize) + ox) * pixels.width + ((y * ybsize) + oy)) * 4;
-					make_block_array[y * total_wblocks + x].data[(oy * xbsize + wx) * 4 + 0] := pixels.data[pix];
-					make_block_array[y * total_wblocks + x].data[(oy * xbsize + wx) * 4 + 1] := pixels.data[pix + 1];
-					make_block_array[y * total_wblocks + x].data[(oy * xbsize + wx) * 4 + 2] := pixels.data[pix + 2];
-					make_block_array[y * total_wblocks + x].data[(oy * xbsize + wx) * 4 + 3] := pixels.data[pix + 3];
-					dec(wx);
+					{ subtract from pixels.width or height to flip the imag  correctly }
+					pix := ( ( pixels.width - ((x * xbsize) + ox) ) + ( pixels.height - ((y * ybsize) + oy) ) * pixels.width ) * 4;
+					MakeBlockArray[y * totalWBlocks + x].data[(oy * xbsize + wx) * 4 + 0] := pixels.data[pix];
+					MakeBlockArray[y * totalWBlocks + x].data[(oy * xbsize + wx) * 4 + 1] := pixels.data[pix + 1];
+					MakeBlockArray[y * totalWBlocks + x].data[(oy * xbsize + wx) * 4 + 2] := pixels.data[pix + 2];
+					MakeBlockArray[y * totalWBlocks + x].data[(oy * xbsize + wx) * 4 + 3] := pixels.data[pix + 3];
+					Dec(wx);
 				end;
 			end;
 
-			dec(wy);
+			Dec(wy);
 		end;
 	end;
+
+	WriteLn;
 end;
 
-procedure fluten(pixels: TTexture);
+procedure Fluten(pixels: TTexture);
 var
-	sock_addr			: sockaddr;
+	sockAddress			: sockaddr;
 	sock				: LongInt;
 	command				: String;
-	x, y, pix, fix		: UInt64;
-	xoff				: UInt64;
-	yoff				: Real;
-	wix, saved_wix		: UInt64;
-		//		: TStringDynArray;
-	frames				: array of TStringDynArray;
-	raw_blocks			: TBlockArray;
+	x, y, pix			: UInt64;
+	wix					: UInt64;
+	commandList			: TStringDynArray;
+	rawBlocks			: TBlockArray;
 	block				: TBlock;
 begin
+	if ParamCount < 2 then
+	begin
+		WriteLn('USAGE: fpf <IP> <PORT>');
+		Halt(1);
+	end;
+
 	sock := fpSocket(AF_INET, SOCK_STREAM, 0);
 	if sock = -1 then
 	begin
-		writeln('fpSocket');
-		halt(1);
-	end;
-
-	sock_addr.sin_family := AF_INET;
-	sock_addr.sin_port := htons(1234);
-	sock_addr.sin_addr.s_addr := StrToNetAddr('151.219.13.203').s_addr;
-
-	x := 0;
-
-	raw_blocks := make_block_array(pixels);
-	shuffle_block_array(raw_blocks);
-	SetLength(frames, 192);
-
-	writeln('block count ', Length(raw_blocks));
-
-	wix := 0;
-	for block in raw_blocks do
-	begin
-		writeln('block xorg ', block.xorg, ' yorg ', block.yorg);
-		if Length(block.data) = 0 then
-		begin
-			halt(1);
-			writeln('nil len, xorg ', block.xorg, ' yorg ', block.yorg);
-			continue;
-		end;
-
-		xoff := 0;
-		yoff := 1080 / 2;
-		saved_wix := wix;
-		for fix := 0 to High(frames) do
-		begin
-			wix := saved_wix;
-			SetLength(frames[fix], pixels.width * pixels.height + Length(raw_blocks));
-			frames[fix][wix] := Format('OFFSET %d %d'#10, [xoff+block.yorg,round(yoff)+block.xorg]);
-			Inc(wix);
-
-			for y := 0 to block.h - 1 do
-			begin
-				for x := 0 to block.w - 1 do
-				begin
-					pix := (y * block.w + x) * 4;
-					if block.data[pix+3] = 0 then
-						SetLength(frames[fix][wix], 0)
-					else
-					begin
-						frames[fix][wix] := frames[fix][wix] + Format('PX %d %d %.2x%.2x%.2x'#10, [
-							y,
-							x,
-							block.data[pix+2],
-							block.data[pix + 1],
-							block.data[pix]
-						]);
-					end;
-
-					inc(wix);
-				end;
-			end;
-			yoff := 1080 / 2 + (40 * sin(xoff / 100));
-			xoff += 10;
-			if xoff > 1920 then
-				xoff := 0;
-			end;
-	end;
-
-	writeln('wix ', wix, ' command capcity ', Length(frames[fix]));
-	if fpConnect(sock, @sock_addr, sizeof(sock_addr)) < 0 then
-	begin
-		writeln('fpConnect');
+		WriteLn('[FATAL] fpSocket: ', SocketError);
 		halt(2);
 	end;
 
-	writeln('connected');
+	sockAddress.sin_family := AF_INET;
+	sockAddress.sin_port := htons(StrToInt(ParamStr(2)));
+	if ParamCount < 1 then
+		sockAddress.sin_addr.s_addr := StrToNetAddr(ParamStr(1)).s_addr;
 
-	while True do
+	x := 0;
+
+	rawBlocks := MakeBlockArray(pixels);
+	ShuffleBlockArray(rawBlocks);
+
+	SetLength(commandList, pixels.width * pixels.height + Length(rawBlocks));
+
+	wix := 0;
+	for block in rawBlocks do
 	begin
-		for fix := 0 to High(frames) do
+		if Length(block.data) = 0 then
 		begin
-			for command in frames[fix] do
+			WriteLn('[FATAL] nil len, xorg ', block.xorg, ' yorg ', block.yorg);
+			halt(1);
+		end;
+
+		commandList[wix] := Format('OFFSET %d %d'#10, [block.xorg,block.yorg]);
+		Inc(wix);
+
+		for y := 0 to block.h - 1 do
+		begin
+			for x := 0 to block.w - 1 do
 			begin
-				if Length(command) = 0 then
-					continue;
-				if fpSend(sock, @command[1], Length(command), 0) < 0 then
+				pix := (y * block.w + x) * 4;
+				if block.data[pix+3] = 0 then
+					SetLength(commandList[wix], 0)
+				else
 				begin
-					writeln('fpSend: ', SocketError);
-					halt(123);
+					commandList[wix] := commandList[wix] + Format('PX %d %d %.2x%.2x%.2x'#10, [
+						x,
+						y,
+						block.data[pix+2],
+						block.data[pix + 1],
+						block.data[pix]
+					]);
 				end;
+
+				Inc(wix);
 			end;
 		end;
 	end;
+
+	if fpConnect(sock, @sockAddress, sizeof(sockAddress)) < 0 then
+	begin
+		WriteLn('[FATAL] fpConnect: ', SocketError);
+		halt(2);
+	end;
+
+	WriteLn('connected');
+
+	while True do
+	begin
+		for command in commandList do
+		begin
+			if Length(command) = 0 then
+				continue;
+			if fpSend(sock, @command[1], Length(command), 0) < 0 then
+			begin
+				WriteLn('fpSend: ', SocketError);
+				halt(123);
+			end;
+		end;
+end;
 end;
 
 var
@@ -233,5 +227,5 @@ begin
 			pixels := uBMP.LoadTexture(_resource);
 	end;
 
-	fluten(pixels);
+	Fluten(pixels);
 end.
